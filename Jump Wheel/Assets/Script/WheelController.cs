26a5d04687ebdbb7c7dcba9dcca530b5bfea24aa
JorisDingelstad <36536380+JorisDingelstad@users.noreply.github.com>
2020-01-08 14:16:08 +0100

    push 1
    heb wat stuff in wheelcontroller toegevoegd en dingen aan de scene toegevoegd.

--- Jump Wheel/Assets/Script/WheelController.cs
+++ Jump Wheel/Assets/Script/WheelController.cs
@@ -7,9 +7,13 @@ public class WheelController : PhysicsObject
 
     public float maxSpeed = 7;
     public float jumpTakeOffSpeed = 7;
+    public float chargeForce;
     public bool flipped;
+    public float momentumReduction;
+    public float wallMomentumLoss;
     Vector2 move = Vector2.zero;
     float charge = 0;
+
 
     //public SpriteRenderer spriteRenderer;
     //private Animator animator;
@@ -25,15 +29,31 @@ public class WheelController : PhysicsObject
     {
         move.y = 0;
 
+        if(move.x < 0.001f && move.x > -0.001f)
+        {
+            move.x = 0;
+        }
+
         if(parentHackHitWall)
         {
             move.x *= -1;
             parentHackHitWall = false;
+            gravityVelocity.y = jumpTakeOffSpeed / 2;
+
+            if (move.x > 0)
+            {
+                move.x -= wallMomentumLoss ;
+            }
+            else if (move.x < 0)
+            {
+                move.x += wallMomentumLoss ;
+            }
+
         }
 
         if(Input.GetKey(KeyCode.Space) && move.x == 0)
         {
-            charge += Time.deltaTime;
+            charge += Time.deltaTime * chargeForce;
         }
         else if (Input.GetKeyUp(KeyCode.Space) && move.x == 0)
         {
@@ -49,18 +69,19 @@ public class WheelController : PhysicsObject
             move.x = Mathf.Abs(move.x);
         }
 
+
         if (Input.GetButtonDown("Jump") && grounded && move.x != 0)
         {
             gravityVelocity.y = jumpTakeOffSpeed;
         }
-        else if (Input.GetButtonUp("Jump"))
+        /*else if (Input.GetButtonUp("Jump"))
         {
             if (gravityVelocity.y > 0)
             {
                 gravityVelocity.y *= 0.5f;
             }
         }
-
+*/
         //bool flipSprite = (!flipped ? (move.x > 0f) : (move.x < 0f));
         //if (flipSprite)
         //{
@@ -72,5 +93,30 @@ public class WheelController : PhysicsObject
         //animator.SetFloat("velocityX", Mathf.Abs(velocity.x) / maxSpeed);
 
         targetVelocity = move * maxSpeed;
+
+        if (grounded)
+        {
+            if (move.x > 0)
+            {
+                move.x -= momentumReduction * Time.deltaTime;
+            }
+            else if (move.x < 0)
+            {
+                move.x += momentumReduction * Time.deltaTime;
+            }
+        }
+        else
+        {
+            if (move.x > 0)
+            {
+                move.x -= momentumReduction/2 * Time.deltaTime;
+            }
+            else if (move.x < 0)
+            {
+                move.x += momentumReduction/2 * Time.deltaTime;
+            }
+        }
+
+
     }
 }
